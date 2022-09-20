72d3653c0af26206eb83a2bf9ce2151fd8e35b3a
Ayon Chowdhury <91245849+Ayon090@users.noreply.github.com>
2022-09-20 15:35:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = document.querySelector(".form input"),
+  generateBtn = document.querySelector(".form button"),
+  qrImg = document.querySelector(".qr-code img");
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if (!qrValue) return;
+  generateBtn.innerHTML = "Generating QR Code...";
+  qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue} `; // qr code generator API
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerHTML = "Generate QR Code";
+  });
+});
+qrInput.addEventListener("keyup", () => {
+  if (!qrInput.value) {
+    wrapper.classList.remove("active");
+  }
+});
